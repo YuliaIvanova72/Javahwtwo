2de7599bac4c76ee0c027dbbe4690eb9ef283ba0
Yulia Ivanova <ivanova.yuvl@gmail.com>
2023-02-27 17:28:31 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 6800;
+        int numberOfRublesPerMile = 20;
+        int miles = ticketPrice / numberOfRublesPerMile;
+        System.out.println(miles);
+    }
+}
